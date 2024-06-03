b747baa6889f4bab7fc0b0ea56700f76572e4a43
AnnetteCarolyn <121865388+AnnetteCarolyn@users.noreply.github.com>
2024-06-03 17:35:02 +0200

    upload "pole koła"

---
+++ C#/pole koła/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace pole_koła
+{
+    public class Program
+    {
+        static void Main ()
+        {
+        
+        
+            double liczbaPi = 3.14 ;
+            double radius = 2.5;
+
+            double area = radius * radius * liczbaPi;
+
+            Console.WriteLine(area);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+    
+
